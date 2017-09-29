8512ed12d86189f81a2ea4a9601242b74a2cfbe4
Caleb <nohitokaze@gmail.com>
2017-09-29 17:34:25 -0400

    Minor html fixes ::selection implemented
    Now all that's left to do is the card transistions

--- hw2/javascript/script.js
+++ hw2/javascript/script.js
@@ -7,17 +7,24 @@
 ==========================================================*/
 function loadMain() {
     location.assign('main.html');
-    $('.main-button').css('text-decoration', 'overline');
+    $('.main-button').css('text-decoration', 'underline'); // for some reason the underline doesn't stay
 }
 
 function loadTour() {
     location.assign('tourist.html');
+    $('.tourist-button').css('text-decoration', 'underline');
 }
 
 function loadMap() {
     location.assign('map.html');
+    $('.map-button').css('text-decoration', 'underline');
 }
 
 function pageTransition(){
 
 }
+
+$('.ex-history').mouseenter();
+$('.ex-history').mouseleave();
+$('.geo').mouseenter();
+$('.pol').mouseenter();
