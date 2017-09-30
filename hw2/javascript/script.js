8fe7397c684d2f9a53746b68bbcc0d96973879fd
Caleb <nohitokaze@gmail.com>
2017-09-30 06:08:00 -0400

    jQuery implementation 50% complete
    Card events on main page work almost expected. Now I just need to have
    the X button and the rotation to display the data.

--- hw2/javascript/script.js
+++ hw2/javascript/script.js
@@ -5,26 +5,95 @@
    This Javascript is the external functionality for the akihabara showcase website
 
 ==========================================================*/
+// nav functions, shows underline under current page
 function loadMain() {
-    location.assign('main.html');
-    $('.main-button').css('text-decoration', 'underline'); // for some reason the underline doesn't stay
+    $('.main-button').addClass('focused-button');
+    $('.tourist-button').removeClass('focused-button');
+    $('.map-button').removeClass('focused-button');
 }
 
 function loadTour() {
-    location.assign('tourist.html');
-    $('.tourist-button').css('text-decoration', 'underline');
+    $('.tourist-button').addClass('focused-button');
+    $('.main-button').removeClass('focused-button');
+    $('.map-button').removeClass('focused-button');
 }
 
 function loadMap() {
-    location.assign('map.html');
-    $('.map-button').css('text-decoration', 'underline');
+    $('.map-button').addClass('focused-button');
+    $('.tourist-button').removeClass('focused-button');
+    $('.main-button').removeClass('focused-button');
 }
 
 function pageTransition(){
 
 }
 
-$('.ex-history').mouseenter();
-$('.ex-history').mouseleave();
-$('.geo').mouseenter();
-$('.pol').mouseenter();
+// nav bar functions for getting from page to page
+$(document).on('click', '.main-button', function(){
+    location.assign('main.html');
+});
+$(document).on('click', '.tourist-button', function(){
+    location.assign('tourist.html');
+});
+$(document).on('click', '.map-button', function(){
+    location.assign('map.html');
+});
+
+// grid functions for main page
+$(document).on('mouseenter', '.ex-history', function(){
+    // Raise and scale card by a small factor
+    var card = $('.ex-history');
+    card.addClass('focused-card');
+});
+
+$(document).on('click', '.ex-history', function(){
+    // Raise and scale card by a small factor, perhaps apply float
+    var card = $('.ex-history');
+   card.off();
+   card.removeClass('focused-card');
+   card.addClass('big-card');
+});
+
+$(document).on('mouseleave', '.ex-history', function(){
+    // Raise and scale card by a small factor, perhaps apply float
+    var card = $('.ex-history');
+    card.removeClass('focused-card');
+});
+
+
+
+$(document).on('mouseenter', '.geo', function(){
+    var card = $('.geo');
+    card.addClass('focused-card');
+});
+
+$(document).on('click', '.geo', function(){
+    // Raise and scale card by a small factor, perhaps apply float
+    var card = $('.geo');
+    card.addClass('big-card');
+
+});
+
+$(document).on('mouseleave', '.geo', function(){
+    var card = $('.geo');
+     card.removeClass('focused-card');
+});
+
+
+
+$(document).on('mouseenter', '.pol', function(){
+    var card = $('.pol');
+    card.addClass('focused-card');
+});
+
+$(document).on('click', '.pol', function(){
+    // Raise and scale card by a small factor, perhaps apply float
+    var card = $('.pol');
+    card.addClass('big-card');
+
+});
+
+$(document).on('mouseleave', '.pol', function(){
+    var card = $('.pol');
+    card.removeClass('focused-card');
+});
