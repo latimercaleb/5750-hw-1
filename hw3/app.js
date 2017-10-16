d802f970de77363ceaf998a4d442914246220a65
Caleb <nohitokaze@gmail.com>
2017-10-16 11:15:53 -0400

    Learn html sessions, it'd be helpful for senior project
    Work on this more later

--- hw3/app.js
+++ hw3/app.js
@@ -4,8 +4,9 @@ $(function(){
 });
 
 $('#adminLoginForm').submit(function(){
-    pCode = $('input').val();
-    console.log("User Code is: "+ pCode);
+    var data = $('#adminLoginForm').serialize();
+    dataJSON = JSON.stringify(data);
+    sessionStorage.setItem('loginCred', dataJSON);
 });
 
 $('#clear').click(function(){
@@ -13,7 +14,8 @@ $('#clear').click(function(){
     console.log('Wiped: '+ pCode);
 })
 
-$('#login').submit(function(){
+$('#login').submit(function(e){
+  e.preventDefault();
     if($('input').val() == pCode){
         window.location.assign('cashier.html')
     }
@@ -21,3 +23,9 @@ $('#login').submit(function(){
         $('main').prepend('<h1>Invalid login</h1>');
     }
 });
+
+function handleData(){
+  pCode = JSON.parse(sessionStorage.getItem('loginCred'));
+  pCode=pCode.substring(6,pCode.length);
+  console.log('Credentials are: '+ pCode);
+}
