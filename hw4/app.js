eb9b5829d6b7518f8e4e450b6440d1b6af710c44
Caleb Latimer <nohitokaze@gmail.com>
2017-11-02 04:00:46 -0400

    Added some materialize functionality, need to iron the css out and then work on the jQuery

--- hw4/app.js
+++ hw4/app.js
@@ -1,46 +1,22 @@
 // JavaScript code for cashier page functionality
 
-
-var pCode;
-var entreeList = [['Select Entree ...', 0],['TacoCombo', 10],['QueasadillaCombo',12],['BuritoCombo',12],['Nachos',  12],['Salad', 8],['Taco Platter', 25]];
-var drinkList = [['Select Drink ...', 0],['Water', 0],['Lemonade', 2],['Beer', 4]];
+var entreeList = [['TacoCombo', 10],['QueasadillaCombo',12],['BuritoCombo',12],['Nachos',  12],['Salad', 8],['Taco Platter', 25]];
+var drinkList = [['Water', 0],['Lemonade', 2],['Beer', 4]];
 
 $(function(){
     console.log("Ready?");
+     $('select').material_select();
 });
 
-$('#adminLoginForm').submit(function(){
-    var data = $('#adminLoginForm').serialize();
-    dataJSON = JSON.stringify(data);
-    sessionStorage.setItem('loginCred', dataJSON);
-});
-
-$('#clear').click(function(){
-    pCode = "";
-    console.log('Wiped: '+ pCode);
-})
+// Event Listeners
 
 $('#total').click(calculateTotal);
-
-$('#login').submit(function(e){
-  e.preventDefault();
-    if($('input').val() == pCode){
-        window.location.assign('cashier.html');
-    }
-    else {
-      // prepend needs a sort of fade in
-      if($('#errorMessage').length == 0){
-        $('main').prepend('<h1 id="errorMessage">Invalid login</h1>');
-      }
-      else {
-      // perhaps add a fade in
-      $('#errorMessage').remove();
-      $('main').prepend('<h1 id="errorMessage">Invalid login</h1>');
-    }
-    //$('input').val() = ''; // how to clear form on invalid input
-
-    }
-});
+// Callbacks
+function handleData(){
+  pCode = JSON.parse(sessionStorage.getItem('loginCred'));
+  pCode=pCode.substring(6,pCode.length);
+  console.log('Credentials are: '+ pCode);
+}
 
 function makeFoodList () {
   entreeList.forEach(function(item){
@@ -59,12 +35,6 @@ function makeDrinkList() {
 function makeMenu () {
   makeFoodList();
   makeDrinkList();
-}
-
-function handleData(){
-  pCode = JSON.parse(sessionStorage.getItem('loginCred'));
-  pCode=pCode.substring(6,pCode.length);
-  console.log('Credentials are: '+ pCode);
 }
 
 function calculateTotal(e){
@@ -90,30 +60,23 @@ function calculateTotal(e){
       $('#mealCost').val(function(){
           return mealPrice;
       });
-
       $('#drinkCost').val(function(){
           return drinkPrice;
       });
-
       $('#subTotalCost').val(function(){
           return mealPrice + drinkPrice;
       });
-
       $('#salesTaxCost').val(function(){
           return (parseFloat($('#subTotalCost').val()) * 0.06).toFixed(2);
       });
-
       $('#totalCost').val(function(){
           return (parseFloat($('#subTotalCost').val()) +parseFloat( $('#salesTaxCost').val())).toFixed(2);
       });
-
       $('#tipCost').val(function(){
           return (parseFloat($('#totalCost').val()) * parseFloat((($('#tip').val()))/100)).toFixed(2);
       });
-
       $('#finalTotal').val(function(){
           return (parseFloat($('#totalCost').val()) + parseFloat($('#tipCost').val())).toFixed(2);
       });
-
     }
 }
